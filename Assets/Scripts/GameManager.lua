244be5eb619751a57f3b9ca6ebd88ab14c572f40
LeaderGRL <43090824+LeaderGRL@users.noreply.github.com>
2024-03-20 11:45:20 +0100

    Update : Add a center window inputBox

--- Assets/Scripts/GameManager.lua
+++ Assets/Scripts/GameManager.lua
@@ -3,8 +3,20 @@ require "Player"
 GameManager = {}
 
 function GameManager:New()
-    Player:Create()
-    Player:RegisterEvents()
+    editBox = tgui.EditBox.Create()
+    text = tgui.String("ip")
+    --editBox = tgui.EditBox()
+    print(editBox)
+    gui:Add(editBox, text)
+    sizeX = tgui.Layout(200)
+    sizeY = tgui.Layout(40)
+    centerX = tgui.Layout((window:getSize().x - sizeX:getValue()) / 2)
+    centerY = tgui.Layout((window:getSize().y - sizeY:getValue()) / 2)
+    editBox:SetSize(sizeX, sizeY)
+    editBox:SetPosition(centerX, centerY)
+    --print(window:getSize().x)
+    --Player:Create()
+    --Player:RegisterEvents()
 end
 
 function GameManager:Update(dt)
