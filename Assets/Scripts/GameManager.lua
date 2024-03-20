a6ce8e4472cbf5f9c0cd99c24ec19cab69a3b52c
LeaderGRL <43090824+LeaderGRL@users.noreply.github.com>
2024-03-20 13:07:07 +0100

    Update : refactor MainMenu script and implement edit box return event

--- Assets/Scripts/GameManager.lua
+++ Assets/Scripts/GameManager.lua
@@ -4,19 +4,7 @@ require "MainMenu"
 GameManager = {}
 
 function GameManager:New()
-
       MainMenu:new()
---    editBox = tgui.EditBox.Create()
---    text = tgui.String("ip")
---    gui:Add(editBox, text)
---    sizeX = tgui.Layout(200)
---    sizeY = tgui.Layout(40)
---    centerX = tgui.Layout((window:getSize().x - sizeX:getValue()) / 2)
---    centerY = tgui.Layout((window:getSize().y - sizeY:getValue()) / 2)
---    editBox:SetSize(sizeX, sizeY)
---    editBox:SetPosition(centerX, centerY)
-    --Player:Create()
-    --Player:RegisterEvents()
 end
 
 function GameManager:Update(dt)
--- MainMenu.lua
+++ MainMenu.lua
@@ -1,21 +1,34 @@
-MainMenu = {}
+MainMenu = {
+    editBox = tgui.EditBox.Create()
+}
 
 function MainMenu:new()
-    self:EditBox(tgui.Layout(200), tgui.Layout(40))
+    local inputField = {editBox = tgui.EditBox.Create()}
+    setmetatable(inputField, self)
+    self.__index = self
+    return inputField
 end
 
 function MainMenu:Update(dt)
     
 end
 
-function MainMenu:EditBox(sizeX, sizeY)
-    editBox = tgui.EditBox.Create()
-    text = tgui.String("ip")
-    gui:Add(editBox, text)
-    editBox:SetSize(sizeX, sizeY)
-    centerX = tgui.Layout((window:getSize().x - sizeX:getValue()) / 2)
-    centerY = tgui.Layout((window:getSize().y - sizeY:getValue()) / 2)
-    editBox:SetPosition(centerX, centerY)
+function MainMenu:InitEditBox(sizeX, sizeY)
+    local text = tgui.String("ip")
+    gui:Add(self.editBox, text)
+    self.editBox:SetSize(sizeX, sizeY)
+    local centerX = tgui.Layout((window:getSize().x - sizeX:getValue()) / 2)
+    local centerY = tgui.Layout((window:getSize().y - sizeY:getValue()) / 2)
+    self.editBox:SetPosition(centerX, centerY)
+    
+    -- Connect the return key press event
+    tgui.OnReturnKeyPress(self.editBox, function() self:OnEditBoxReturn() end)
 end
+
+function MainMenu:OnEditBoxReturn()
+    print("Return")
+end
+
+MainMenu:InitEditBox(tgui.Layout(200), tgui.Layout(40))
 
 return MainMenu
