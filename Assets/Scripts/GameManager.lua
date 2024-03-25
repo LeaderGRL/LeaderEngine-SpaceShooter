83ec8000cad67d8d47c6c3e3a1e00e72a738dfbc
LeaderGRL <43090824+LeaderGRL@users.noreply.github.com>
2024-03-25 12:34:37 +0100

    Update : refactor the game manager to become a singleton and add a main menu script

--- Assets/Scripts/GameManager.lua
+++ Assets/Scripts/GameManager.lua
@@ -1,16 +1,43 @@
-require "Player"
-require "MainMenu"
+--GameManager = {
+--    EManager = SceneManager:GetInstance():GetCurrentScene():GetEntityManager(),
+--    RManager = ResourceManager:GetInstance(),
+--    EventManager = EventManager:GetInstance(),
+--}
 
-GameManager = {}
+local function GameManager(super) -- super is optional, if not provided, it will inherit from Object
 
-function GameManager:New()
-      MainMenu:new()
+    local gameManager = {
+        EManager = SceneManager:GetInstance():GetCurrentScene():GetEntityManager(),
+        RManager = ResourceManager:GetInstance(),
+        EventManager = EventManager:GetInstance(),
+        NetworkManager = NetworkManager:GetInstance(),
+    }
+
+    gameManager.__index = gameManager -- failed table lookups on the instances should fallback to the class table, to get methods and fields
+    setmetatable(gameManager, super) -- inherit from super class if super is provided
+
+
+    function gameManager.New(...) -- The constructor 
+        if gameManager._instance then
+            return gameManager._instance -- return the instance if it exists already
+        end
+
+        local instance = setmetatable({}, gameManager) -- make GameManager handle lookup 
+        if instance.ctor then -- constructor of the derived class 
+            instance:ctor() -- call the constructor
+        end
+
+        gameManager._instance = instance -- save the instance created to be returned by the class
+        return instance -- return the instance of the class 
+    end
+
+    function gameManager.Update(dt)
+        --Player:Update(dt)
+    end
+
+    return gameManager -- return the class table to be assigned to a global variable later
 end
 
-function GameManager:Update(dt)
-    --Player:Update(dt)
-    MainMenu:Update(dt)
-end
-
-GameManager:New()
-return GameManager
+local game = GameManager()
+local manager = game.New()
+return manager
--- Assets/Scripts/MainMenu.lua
+++ Assets/Scripts/MainMenu.lua
@@ -1,3 +1,5 @@
+require "GameManager"
+
 MainMenu = {
     editBox = tgui.EditBox.Create()
 }
@@ -10,19 +12,20 @@ function MainMenu:new()
 end
 
 function MainMenu:Update(dt)
-    print(self.editBox:GetText():ToStdString())
+    --print(self.editBox:GetText():ToStdString())
 end
 
 function MainMenu:InitEditBox(sizeX, sizeY)
     local text = tgui.String("ip")
-    print("ttt", text:ToStdString())
     gui:Add(self.editBox, text)
-    self.editBox:SetSize(sizeX, sizeY)
+
     self.editBox:SetDefaultText(text)
     local centerX = tgui.Layout((window:getSize().x - sizeX:getValue()) / 2)
     local centerY = tgui.Layout((window:getSize().y - sizeY:getValue()) / 2)
     self.editBox:SetPosition(centerX, centerY)
-    
+
+    --self.editBox:SetText(text)
+
     -- Connect the return key press event
     tgui.OnReturnKeyPress(self.editBox, function() self:OnEditBoxReturn() end)
         print(self.editBox)
@@ -30,9 +33,11 @@ function MainMenu:InitEditBox(sizeX, sizeY)
 end
 
 function MainMenu:OnEditBoxReturn()
-    --print("Return key was pressed")
-    print(tgui.GetText(self.editBox))
-    --print("text : ", self:editBox:GetText():ToStdString())
+    MainMenu:SendIP()
+end
+
+function MainMenu:SendIP()
+    gameManager.NetworkManager:SetIP(self.editBox:GetText():ToStdString())
 end
 
 MainMenu:InitEditBox(tgui.Layout(200), tgui.Layout(40))
